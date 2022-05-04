0354d8098fc2d660969053c79a15992fb36f12e1
Remi <75871744+remireems@users.noreply.github.com>
2022-05-03 20:20:16 -0700

    code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import './App.css';
-
 function App() {
+
+  const api = {
+    key: '04a3de3d38e1f0efbd3cb73d312aa4b6',
+    url: 'api.openweathermap.org/data/2.5/forecast/'
+  }
+
   return (
-    <div className="App">
+    <div className="app">
       hello
     </div>
   );
