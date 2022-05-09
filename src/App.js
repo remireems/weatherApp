e0ddecac53af7b245a47ea3275e911717774c256
Remi <75871744+remireems@users.noreply.github.com>
2022-05-09 16:56:45 -0700

    added date and styling

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
 
+  const current = new Date()
+  const date = `${current.getMonth()+1}/${current.getDate()}`
+
   const apiKey = '04a3de3d38e1f0efbd3cb73d312aa4b6'
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${apiKey}`
 
@@ -35,6 +38,7 @@ function App() {
 
       <div className='weatherInfo'>
         <h1 className='wName'>{data.name}</h1>
+        <p className='wDate'>{date}</p>
         {data.weather ? <img className='wImg' src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt={data.weather[0].main} /> : null}
         {data.weather ? <h2 className='weather'>{data.weather[0].main}</h2> : null}
         {data.main ? <h2 className='wTemp'>{data.main.temp.toFixed()}°F</h2> : null}
